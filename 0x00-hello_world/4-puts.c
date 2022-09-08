0e97dee05fb880a3c8a7352c64b0ae1e5fbe3a8f
kenyking <kayodeodunniga@gmail.com>
2022-09-08 15:41:17 -0700

    Task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ *  main -entry point
+ *
+ *  Return: 0after printing function
+ *  */
+#include <stdio.h>
+int main(void)
+{
+	puts("\ "Programming is like building a multilingual puzzle");
+	 return(0);
+}
