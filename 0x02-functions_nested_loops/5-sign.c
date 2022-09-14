395d7519ebfe6626c017179c21be50d3f42ba6e5
kenyking <kayodeodunniga@gmail.com>
2022-09-13 18:11:37 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,12 +9,12 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+')
+		_putchar('+');
 			return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('-')
+		_putchar('-');
 			return (-1);
 	}
 	else
@@ -23,3 +23,4 @@ int print_sign(int n)
 		return (0);
 	}
 }
+
