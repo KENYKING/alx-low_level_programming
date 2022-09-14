78739abf2690d4dcd80544e35a09f542bb090d9a
kenyking <kayodeodunniga@gmail.com>
2022-09-13 18:57:52 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
 * print_last_digit - function that prints the last digit of a number
-* @n: the num in quetion
+* @n: the number in quetion
 * Return: value of the last digit
 */
 int print_last_digit(int n)
@@ -11,6 +11,7 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 		last_digit *= -1;
-	_putchar(last_digit * '0');
+
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
