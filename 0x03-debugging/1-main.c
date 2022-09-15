484192671f230db386372629a9faafb69624f868
kenyking <kayodeodunniga@gmail.com>
2022-09-15 10:50:49 -0700

    Task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,12 +4,10 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
-
 {
 	int i;
-
+	
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
@@ -19,7 +17,6 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
