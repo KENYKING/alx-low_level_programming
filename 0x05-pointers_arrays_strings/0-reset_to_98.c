eb44c4ca5dcbb68578d259fd032ca502eefa6254
kenyking <kayodeodunniga@gmail.com>
2022-09-19 10:17:58 -0700

    Task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - function that takes a pointer to an int
+*
+* Return: Always 0
+*/
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = 98;
+	print("Value of 'p' : %d\n", *p);
+
+	return (0);
+}
